09aad6bec357efa4e31d0b13dd459d15d896cf05
Carl148Carl148 <Carl148Carl148@users.noreply.github.com>
2022-12-19 18:22:39 -0800

    Added Fluen Assertiions

--- AppliToolsProject/UnitTest1.cs
+++ AppliToolsProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -14,6 +15,8 @@ namespace AppliToolsProject
         {
             IWebDriver driver = new ChromeDriver(@"C:\Users\Admin\source\repos\AppliToolsProject\AppliToolsProject\bin\Debug\net6.0\chromedriver");
             driver.Navigate().GoToUrl("https://ultimateqa.com/automation");
+            driver.FindElement(By.LinkText("Fake Pricing Page")).Click();
+            driver.Url.Should().Be("https://ultimateqa.com/automation/fake-pricing-page/");
             driver.Quit();
         }
     }
